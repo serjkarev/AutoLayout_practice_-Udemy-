9f727d8f300873edf98a0af404ed5b2e0208b54a
Serhii KAREV <skarev@e2r7p5.unit.ua>
2019-05-20 16:22:14 +0300

    Introduction to AutoLayout practice

---
+++ Auto layout Calculator/Auto layout Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Auto layout Calculator
+//
+//  Created by Serhii KAREV on 5/19/19.
+//  Copyright © 2019 Serhii KAREV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+}
+
